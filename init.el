a24719c0fe10f37e3d96415d4f767217080a226f
tianheg <i@tianheg.co>
2024-09-01 10:48:46 +0800

    add markdown-mode

--- init.el
+++ init.el
@@ -145,3 +145,10 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
