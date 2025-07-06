bea05de211aa8adda347748c417f00dd11efe071
tianheg <i@tianheg.co>
2025-07-06 11:16:37 +0800

    feat: custom doc config

--- core/init-document.el
+++ core/init-document.el
@@ -9,24 +9,26 @@
   :ensure t
   :defer t)
 
-;; https://emacs.stackexchange.com/questions/17710/use-package-with-config-to-set-variables
 (use-package org
   :pin nongnu
   :mode (("\\.org\\'" . org-mode))
   :ensure org-contrib
-  :bind (("M-p" . org-previous-visible-heading)
-	  ("M-n" . org-next-visible-heading))
+  :bind
+  (("M-p" . org-previous-visible-heading)
+	  ("M-n" . org-next-visible-heading)
+	  ("C-c l" . org-store-link)
+	  ("C-c a" . org-agenda)
+	  ("C-c c" . org-capture))
   :init
-  (setq org-ellipsis " ▾"
-        org-adapt-indentation nil
+  (setq org-adapt-indentation nil
 		org-log-done t
 		org-src-tab-acts-natively nil
 		org-pretty-entities t
 		org-hide-emphasis-markers t
 		org-startup-folded t
-		org-startup-with-inline-images t
-		org-image-actual-width '(1024)
-        org-capture-templates nil)
+    org-capture-templates nil
+    org-agenda-files '("~/Documents/daily/")
+    )
   :config
   ;; GTD setting
   (require 'org-inlinetask)
@@ -82,9 +84,6 @@
 
   (require 'org-indent)
 
-  (use-package ob-go
-    :ensure t)
-
   ;; programming languages
   (org-babel-do-load-languages
    'org-babel-load-languages
@@ -92,25 +91,12 @@
 	 (css . t)
 	 (js . t)
 	 (org . t)
-	 (python . t)
-	 (sed . t)
-	 (sql . t)
-	 (R . t)
-	 (go . t)))
+	 (python . t)))
   )
 
 (use-package toc-org
   :ensure t
   :hook (org-mode . toc-org-mode))
-
-(use-package org-superstar
-  :ensure t
-  :hook (org-mode . org-superstar-mode)
-  :init
-  (setq org-superstar-special-todo-items nil
-		org-superstar-prettify-item-bullets t
-        org-superstar-remove-leading-stars t)
-  (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
 
 (use-package org-appear
   :ensure t
